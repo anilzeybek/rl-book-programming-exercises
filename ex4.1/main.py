24de5207c8ce482ddd16b8887960f90972558e9d
Anil Zeybek <anil.zeybek98@gmail.com>
2020-03-02 02:24:27 +0300

    fix bug on reward process

--- ex4.1/main.py
+++ ex4.1/main.py
@@ -7,38 +7,35 @@ def reward(current_state, action):
     elif action == 0:
         new_state = current_state - 4 if current_state > 3 else current_state
     elif action == 1:
-        new_state = current_state + 1 if current_state != 3 or current_state != 7 or current_state != 11 or current_state != 15 else current_state
+        new_state = current_state + 1 if current_state != 3 and current_state != 7 and current_state != 11 and current_state != 15 else current_state
     elif action == 2:
         new_state = current_state + 4 if current_state < 12 else current_state
     else:
-        new_state = current_state - 1 if current_state != 0 or current_state != 4 or current_state != 8 or current_state != 12 else current_state
+        new_state = current_state - 1 if current_state != 0 and current_state != 4 and current_state != 8 and current_state != 12 else current_state
 
-    if new_state == 0 or new_state == 15:
-        return new_state, 0
-    else:
-        return new_state, -1
+    return new_state, -1
 
 
 def main():
     actions = {0: 'up', 1: 'right', 2: 'down', 3: 'left'}
-    gamma = 1
+    discount_factor = 1
+
     grid_world = np.zeros(16)
-    copy_table = grid_world.copy()
+    copy_world = grid_world.copy()
 
-    theta = 0.0001
-    delta = 0
+    theta = 0.00001
     while True:
-        for state in range(len(grid_world)):
-            grid_world[state] = copy_table[state]
+        delta = 0
 
-            sum_of_actions = 0
+        for state in range(len(grid_world)):
             for a in actions:
                 new_state, rew = reward(state, a)
-                sum_of_actions += 0.25 * (rew + gamma * copy_table[new_state])
+                copy_world[state] += 0.25 * (rew + discount_factor * grid_world[new_state])
 
-            copy_table[state] = sum_of_actions
+            delta = max(delta, np.abs(grid_world[state] - copy_world[state]))
 
-            delta = abs(np.sum(grid_world - copy_table))
+        grid_world = copy_world.copy()
+        copy_world = np.zeros(16)
 
         if delta < theta:
             break
